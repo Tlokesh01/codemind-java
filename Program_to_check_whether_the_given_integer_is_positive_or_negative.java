0893632f3fa08f237f95763d48dfb324f00daaff
Lokesh Kumar Talluri <102519379+Tlokesh01@users.noreply.github.com>
2022-11-06 07:53:37 +0530

    import java.util.*; class VAMPIRE{     public static void main(String args[]){         Scanner in = new Scanner(System.in);         int a = in.nextInt();         if(a<0)System.out.println("Negative Number");         else System.out.println("Positive Number");     } }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class VAMPIRE{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if(a<0)System.out.println("Negative Number");
+        else System.out.println("Positive Number");
+    }
+}
